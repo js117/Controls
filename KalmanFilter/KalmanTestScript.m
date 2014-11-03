90f695d4762498c545470ac6e3f03f8c5c0feed2
unknown <JamesS@JamesS-HP.(none)>
2014-11-02 19:52:53 -0500

    Updated text documentation on Kalman Filter: done version 1.0

--- KalmanFilter/KalmanTestScript.m
+++ KalmanFilter/KalmanTestScript.m
@@ -109,38 +109,42 @@ angles_noisy = x_state_noisy(1,:);
 
 x0bar = x0;
 S0 = 0; % we start (and expect to start) from a state of rest for this process
-V = 0.001*gsigma; % in practice we don't know gsigma exactly, so let's say we underestimate it
+V = 100; % in practice we don't know gsigma exactly, so let's say we underestimate it
 vbar = gbar;
-W = 0.001*[1,0;0,10]; %"process noise"
+W = 1*[1,0;0,1]; %"process noise"
 wbar = 0;
 y_data = zeros(p,numTimeSteps);
 y_data(1,:) = g;
-%[x_kalman, y_error] = KalmanTestRoutine(S0, x0bar, W, wbar, V, vbar, A, C, y_data, numTimeSteps);
+
+% We can use this one line to call the Kalman Filter routine, or the
+% commented-out below version which is pretty much a verbatim copy of that
+% code. 
+[x_kalman, y_error] = KalmanTestRoutine(S0, x0bar, W, wbar, V, vbar, A, C, y_data, numTimeSteps);
 
 % Below code adapted from KalmanTestRoutine:
-x_kalman = zeros(n, numTimeSteps); %i.e. every column will be an updated state snapshot
-y_error = zeros(p, numTimeSteps); %ditto for y; note this is also the size of y_data as given
-x_hat_previous = x0bar;
-x_hat_measured = zeros(size(x0bar));
-x_hat_updated = zeros(size(x0bar));
-S_previous = S0;
-% Simulation. For a real-time system, this loop runs continually.
-for i=1:numTimeSteps
-
-   % Apply measurement and time updates: 
-   [x_hat_measured, S_measured] = KalmanMeasurementUpdate(x_hat_previous, S_previous, C, V, y_data(1,i), vbar);
-   [x_hat_updated, S_updated] = KalmanTimeUpdate(x_hat_measured, S_measured, A, W, wbar);
-
-   % Recrod the Kalman filtered state estimate, and the measurement error: 
-   % (in application code, this is where we look at quantities of interest)
-   x_kalman(:,i) = x_hat_updated; %or take a look at x_hat_updated
-   y_error(:,i) = y_data(:,i) - C*x_hat_measured - vbar; 
-
-   % Update recursively for next iteration:
-   S_previous = S_updated;
-   x_hat_previous = x_hat_updated;
-   
-end
+% x_kalman = zeros(n, numTimeSteps); %i.e. every column will be an updated state snapshot
+% y_error = zeros(p, numTimeSteps); %ditto for y; note this is also the size of y_data as given
+% x_hat_previous = x0bar;
+% x_hat_measured = zeros(size(x0bar));
+% x_hat_updated = zeros(size(x0bar));
+% S_previous = S0;
+% % Simulation. For a real-time system, this loop runs continually.
+% for i=1:numTimeSteps
+% 
+%    % Apply measurement and time updates: 
+%    [x_hat_measured, S_measured] = KalmanMeasurementUpdate(x_hat_previous, S_previous, C, V, y_data(1,i), vbar);
+%    [x_hat_updated, S_updated] = KalmanTimeUpdate(x_hat_measured, S_measured, A, W, wbar);
+% 
+%    % Recrod the Kalman filtered state estimate, and the measurement error: 
+%    % (in application code, this is where we look at quantities of interest)
+%    x_kalman(:,i) = x_hat_updated; %or take a look at x_hat_updated
+%    y_error(:,i) = y_data(:,i) - C*x_hat_measured - vbar; 
+% 
+%    % Update recursively for next iteration:
+%    S_previous = S_updated;
+%    x_hat_previous = x_hat_updated;
+%    
+% end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % 4. Plot and review results 
